85a0c1411b491f6d3a5db534213acc2ad61ee079
Davdi <n.davdi@gmail.com>
2020-12-18 21:14:31 +0100

    kész

--- assets/app.js
+++ assets/app.js
@@ -2,12 +2,12 @@ import * as pictureObject from './module.js';
 
 console.log(pictureObject);
 
-// const pictureAdder = () => {
+const pictureAdder = () => {
     document.getElementById("i1").setAttribute("src", pictureObject.img1);
     document.getElementById("i2").setAttribute("src", pictureObject.img2);
     document.getElementById("i3").setAttribute("src", pictureObject.img3);
     document.getElementById("i4").setAttribute("src", pictureObject.img4);
     document.getElementById("i5").setAttribute("src", pictureObject.img5);
-// };
+};
 
-// pictureAdder();
+pictureAdder();
--- assets/module.js
+++ assets/module.js
@@ -1,8 +1,8 @@
 const img1 = "https://www.w3schools.com/howto/img_nature_wide.jpg"
-const img2 = "https://www.w3schools.com/howto/img_nature_wide.jpg"
-const img3 = "https://www.w3schools.com/howto/img_nature_wide.jpg"
-const img4 = "https://www.w3schools.com/howto/img_nature_wide.jpg"
-const img5 = "https://www.w3schools.com/howto/img_nature_wide.jpg"
+const img2 = "https://www.w3schools.com/howto/img_mountains_wide.jpg"
+const img3 = "https://www.w3schools.com/howto/img_lights_wide.jpg"
+const img4 = "https://www.w3schools.com/howto/img_snow_wide.jpg"
+const img5 = "https://www.vereb.hu/images/szervezetek/reformatus/reftemp04.jpg"
 
 export {
     img1,
